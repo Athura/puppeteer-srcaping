96ad036c2a707943ffd2ef8710f3f369c96a9ea8
Athura <joshualjohnson33@gmail.com>
2019-03-28 07:50:26 -0400

    need to figure out why we're timing out all over the place.

--- starbucks-scrape.js
+++ starbucks-scrape.js
@@ -6,6 +6,20 @@ const fs = require("fs");
 const error = chalk.bold.red;
 const success = chalk.keyword("green");
 
+// Helper function to make xpaths available for targeting
+// https://stackoverflow.com/questions/48476356/is-there-a-way-to-add-script-to-add-new-functions-in-evaluate-context-of-chrom
+const helperFunctions = () => {
+  window.$x = xPath => document
+    .evaluate(
+      xPath,
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    )
+    .singleNodeValue;
+};
+
 // This at a later time can be turned into an async function so we can call it inside an endpoint when we want new data. We can also add this to a cron job to run weekly for new jsons
 (async () => {
     try {
@@ -13,29 +27,50 @@ const success = chalk.keyword("green");
       const browser = await puppeteer.launch({ headless: true });
       // open a new page
       const page = await browser.newPage();
+      console.log(success("Browser Created"));
       // enter url in page
       await page.goto(`https://athome.starbucks.com/blonde-roast-coffees/`);
-      await page.waitForSelector("div.sub-hero-title");
 
-      let titleNodeList = page.$x("//div[4]/div/p[2]").innerText;
-      console.log(titleNodeList);
+      // https://github.com/GoogleChrome/puppeteer/issues/2917
+      // We have to craft our own idle detector
+      // await page.waitForSelector("div.sub-hero-title");
+      let inflightImageRequests = 0;
+      page.on('request', request => {
+        if (request.resourceType() !== 'image')
+          return;
+        ++inflightImageRequests;
+      });
+      console.log(success("All image requests are beginning!"));
+      page.on('requestfinished', requestFinished);
+      page.on('requestfailed', requestFinished);
+      function requestFinished(request) {
+        if (request.resourceType() !== 'image')
+         return;
+        inflightImageRequests = Math.max(inflightImageRequests - 1, 0);
+        if (inflightImageRequests)
+          return;
+        console.log(success("All image requests are complete!"));
+      }
+      console.log(success("hero found"));
 
       const results = await page.evaluate(() => {
-        let titleNodeList = document.querySelectorAll(`a.storylink`);
-        let ageList = document.querySelectorAll(`span.age`);
-        let scoreList = document.querySelectorAll(`span.score`);
+        console.log(success("enter eval"));
+        let titleNodeList = $x("//div[2]/div/a/h3");
+        let coffeeNodeLinkList = $x("//div[2]/div/a");
+        let descNodeList = $x("//div[4]/div/p[2]");
+        let coffeeImgList = $("//div[1]/div/a/img");
         let titleLinkArray = [];
         for (let i = 0; i < titleNodeList.length; i++) {
           titleLinkArray[i] = {
             title: titleNodeList[i].innerText.trim(),
-            link: titleNodeList[i].getAttribute("href"),
-            age: ageList[i].innerText.trim(),
-            score: scoreList[i].innerText.trim()
+            link: coffeeNodeLinkList[i].getAttribute("href"),
+            age: descNodeList[i].innerText.trim(),
+            score: coffeeImgList[i].getAttribute("src")
           };
         }
         return titleLinkArray;
       });
-      // console.log(news);
+
       await browser.close();
       // Writing the news inside a json file
       fs.writeFile("starbucks.json", JSON.stringify(results), function(err) {
--- ycombinator-scrape.js
+++ ycombinator-scrape.js
@@ -1,49 +1,51 @@
 const puppeteer = require("puppeteer");
 const chalk = require("chalk");
-const fs = require("fs");
+var fs = require("fs");
 
-// Add some color for debugging purposes
+// MY OCD of colorful console.logs for debugging... IT HELPS
 const error = chalk.bold.red;
 const success = chalk.keyword("green");
 
-// This at a later time can be turned into an async function so we can call it inside an endpoint when we want new data. We can also add this to a cron job to run weekly for new jsons
 (async () => {
-    try {
-      // open the headless browser
-      const browser = await puppeteer.launch({ headless: true });
-      // open a new page
-      const page = await browser.newPage();
-      // enter url in page
-      await page.goto(`https://news.ycombinator.com/`);
-      await page.waitForSelector("a.storylink");
+  try {
+    // open the headless browser
+    var browser = await puppeteer.launch({ headless: true });
+    // open a new page
+    var page = await browser.newPage();
+    // enter url in page
+    await page.goto(`https://news.ycombinator.com/`, {
+      timeout: 0,
+      waitUntil: 'networkidle2'
+    });
+    await page.waitForSelector("a.storylink");
 
-      const results = await page.evaluate(() => {
-        let titleNodeList = document.querySelectorAll(`a.storylink`);
-        let ageList = document.querySelectorAll(`span.age`);
-        let scoreList = document.querySelectorAll(`span.score`);
-        let titleLinkArray = [];
-        for (let i = 0; i < titleNodeList.length; i++) {
-          titleLinkArray[i] = {
-            title: titleNodeList[i].innerText.trim(),
-            link: titleNodeList[i].getAttribute("href"),
-            age: ageList[i].innerText.trim(),
-            score: scoreList[i].innerText.trim()
-          };
-        }
-        return titleLinkArray;
-      });
-      // console.log(news);
-      await browser.close();
-      // Writing the news inside a json file
-      fs.writeFile("hackernews.json", JSON.stringify(results), function(err) {
-        if (err) throw err;
-        console.log("Saved!");
-      });
-      console.log(success("Browser Closed"));
-    } catch (err) {
-      // Catch and display errors
-      console.log(error(err));
-      await browser.close();
-      console.log(error("Browser Closed"));
-    }
-  })();
+    var news = await page.evaluate(() => {
+      var titleNodeList = document.querySelectorAll(`a.storylink`);
+      var ageList = document.querySelectorAll(`span.age`);
+      var scoreList = document.querySelectorAll(`span.score`);
+      var titleLinkArray = [];
+      for (var i = 0; i < titleNodeList.length; i++) {
+        titleLinkArray[i] = {
+          title: titleNodeList[i].innerText.trim(),
+          link: titleNodeList[i].getAttribute("href"),
+          age: ageList[i].innerText.trim(),
+          score: scoreList[i].innerText.trim()
+        };
+      }
+      return titleLinkArray;
+    });
+    // console.log(news);
+    await browser.close();
+    // Writing the news inside a json file
+    fs.writeFile("hackernews.json", JSON.stringify(news), function(err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
+    console.log(success("Browser Closed"));
+  } catch (err) {
+    // Catch and display errors
+    console.log(error(err));
+    await browser.close();
+    console.log(error("Browser Closed"));
+  }
+})();
